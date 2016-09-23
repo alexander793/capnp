99e6f22e84ce33a0e602472da0e836ace8ea0fd9
alex <heilwodan@web.de>
2016-09-23 09:34:42 +0200

    commit 1 to reorder structure

---
+++ CapnpBackup/runtime/src/main/java/org/capnproto/BufferedOutputStreamWrapper.java
@@ -0,0 +1,98 @@
+// Copyright (c) 2013-2014 Sandstorm Development Group, Inc. and contributors
+// Licensed under the MIT License:
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package org.capnproto;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.WritableByteChannel;
+
+
+public final class BufferedOutputStreamWrapper implements BufferedOutputStream {
+
+	private final WritableByteChannel inner;
+	private final ByteBuffer buf;
+
+	public BufferedOutputStreamWrapper(WritableByteChannel w) {
+		this.inner = w;
+		this.buf = ByteBuffer.allocate(8192);
+	}
+
+	public final int write(ByteBuffer src) throws IOException {
+		int available = this.buf.remaining();						// number of free bytes in this.buffer
+		int size = src.remaining();									// number of bytes to be written from the src buffer to this.buffer
+		if (size <= available) {									// if there is enough space left in this.buffer for the bytes from the src buffer
+			this.buf.put(src);										// writes the bytes from the src to this.buffer
+		} else if (size <= this.buf.capacity()) {								//if this.buffer will be full with the additional bytes from the src buffer and must be written to the channel
+			//# Too much for this buffer, but not a full buffer's worth,		// and the src buffer would fill a whole new buffer
+			//# so we'll go ahead and copy.
+			ByteBuffer slice = src.slice();
+			slice.limit(available);
+			this.buf.put(slice);									// fills the free space of this.buffer with bytes from the src buffer
+
+			this.buf.rewind();										// sets the position of this buffer to its beginning
+			while (this.buf.hasRemaining()) {						// as long as there are bytes left in this.buffer to write on the channel
+				this.inner.write(this.buf);							// writes this buffer to the channel
+			}
+			this.buf.rewind();										// all bytes are written, so the position is set back to the beginning of the buffer
+
+			src.position(src.position() + available);				// updates the pointer of the src buffer to the next byte that has to be written
+			this.buf.put(src);										// fills the fresh buffer with the rest of the src buffer
+			// won't take the whole space of the buffer, because of the IF-statement
+		} else {													// if the src buffer is bigger than this buffers capacity, it wouldn't make sense to copy all the data first to a buffer and then rite it to the channel
+			// so we write all the bytes from the src buffer directly to the channel
+			//# Writing so much data that we might as well write
+			//# directly to avoid a copy.
+
+			int pos = this.buf.position();							//saves the position of the last filled byte of the buffer
+			this.buf.rewind();										// sets the position of the buffer to its beginning
+			ByteBuffer slice = this.buf.slice();
+			slice.limit(pos);										// creates a new buffer as big as the actual content of this.buffer -> empty space of this.buffer is cut off
+			while (slice.hasRemaining()) {
+				this.inner.write(slice);							// writes the filled bytes of this.buffer to the channel
+			}
+			while (src.hasRemaining()) {
+				this.inner.write(src);								// writes the bytes from the src buffer to the chanel
+			}
+		}
+		return size;												//returns the number of bytes of the source channel -> maybe it should be updated during the write process ?
+	}
+
+	public final ByteBuffer getWriteBuffer() {
+		return this.buf;
+	}
+
+	public final void close() throws IOException {
+		this.inner.close();
+	}
+
+	public final boolean isOpen() {
+		return this.inner.isOpen();
+	}
+
+	public final void flush() throws IOException {					// writes all the filled bytes of this.buffer to the channel and clears the buffer
+		int pos = this.buf.position();
+		this.buf.rewind();
+		this.buf.limit(pos);
+		this.inner.write(this.buf);
+		this.buf.clear();
+	}
+}
---
+++ CapnpBackup/runtime/src/main/java/org/capnproto/ListReader.java
@@ -0,0 +1,119 @@
+// Copyright (c) 2013-2014 Sandstorm Development Group, Inc. and contributors
+// Licensed under the MIT License:
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package org.capnproto;
+
+public class ListReader {
+
+	public interface Factory<T> {
+
+		T constructReader(SegmentReader segment, int ptr, int elementCount, int step, int structDataSize,
+				short structPointerCount, int nestingLimit);
+	}
+
+	final SegmentReader segment;
+	final int ptr; // byte offset to front of list		// offset to the first element of list
+	final int elementCount;								// amount of elements in the list
+	final int step; // in bits							// probably the size of each element
+	final int structDataSize; // in bits				// size of the actual data of the List
+	final short structPointerCount;						// number of pointers in the list
+	final int nestingLimit;								// maybe the max size of the list ?
+
+	public ListReader() {
+		this.segment = null;
+		this.ptr = 0;
+		this.elementCount = 0;
+		this.step = 0;
+		this.structDataSize = 0;
+		this.structPointerCount = 0;
+		this.nestingLimit = 0x7fffffff;
+	}
+
+	public ListReader(SegmentReader segment, int ptr, int elementCount, int step, int structDataSize, short structPointerCount,
+			int nestingLimit) {
+		this.segment = segment;
+		this.ptr = ptr;
+		this.elementCount = elementCount;
+		this.step = step;
+		this.structDataSize = structDataSize;
+		this.structPointerCount = structPointerCount;
+		this.nestingLimit = nestingLimit;
+
+	}
+
+	public int size() {
+		return this.elementCount;
+	}
+
+	protected boolean _getBooleanElement(int index) {														//checks if a specified bit of a list element is set or not
+		long bindex = (long) index * this.step;
+		byte b = this.segment.buffer.get(this.ptr + (int) (bindex / Constants.BITS_PER_BYTE));
+		return (b & (1 << (bindex % 8))) != 0;
+	}
+
+	protected byte _getByteElement(int index) {																					// reads the bytes of the element at the given index
+		return this.segment.buffer.get(this.ptr + (int) ((long) index * this.step / Constants.BITS_PER_BYTE));
+	}
+
+	protected short _getShortElement(int index) {																				// reads the bytes of the element at the given index
+		return this.segment.buffer.getShort(this.ptr + (int) ((long) index * this.step / Constants.BITS_PER_BYTE));				// and transforms them into a short value
+	}
+
+	protected int _getIntElement(int index) {																					// reads the bytes of the element at the given index
+		return this.segment.buffer.getInt(this.ptr + (int) ((long) index * this.step / Constants.BITS_PER_BYTE));					// and transforms them into an int value
+	}
+
+	protected long _getLongElement(int index) {																					// reads the bytes of the element at the given index
+		return this.segment.buffer.getLong(this.ptr + (int) ((long) index * this.step / Constants.BITS_PER_BYTE));				// and transforms them into a long value
+	}
+
+	protected float _getFloatElement(int index) {																				// reads the bytes of the element at the given index
+		return this.segment.buffer.getFloat(this.ptr + (int) ((long) index * this.step / Constants.BITS_PER_BYTE));				// and transforms them into a float value
+	}
+
+	protected double _getDoubleElement(int index) {																				// reads the bytes of the element at the given index
+		return this.segment.buffer.getDouble(this.ptr + (int) ((long) index * this.step / Constants.BITS_PER_BYTE));				// and transforms them into a double value
+	}
+
+	protected <T> T _getStructElement(StructReader.Factory<T> factory, int index) {												//creates a new Reader for a struct object
+		// TODO check nesting limit
+
+		long indexBit = (long) index * this.step;												// beginning of the struct
+		int structData = this.ptr + (int) (indexBit / Constants.BITS_PER_BYTE);					// beginning of the DataSection of the struct
+		int structPointers = structData + (this.structDataSize / Constants.BITS_PER_BYTE);		// beginning of the PointerSection of the struct
+
+		return factory.constructReader(this.segment, structData, structPointers / 8, this.structDataSize,
+				this.structPointerCount, this.nestingLimit - 1);
+	}
+
+	protected <T> T _getPointerElement(FromPointerReader<T> factory, int index) {												//creates a new Reader for a pointer Object
+		return factory.fromPointerReader(this.segment, (this.ptr + (int) ((long) index * this.step / Constants.BITS_PER_BYTE))
+				/ Constants.BYTES_PER_WORD, this.nestingLimit);
+	}
+
+	protected <T> T _getPointerElement(FromPointerReaderBlobDefault<T> factory, int index,										//creates a new Reader for a pointer Object
+			java.nio.ByteBuffer defaultBuffer, int defaultOffset, int defaultSize) {
+		return factory.fromPointerReaderBlobDefault(this.segment,
+				(this.ptr + (int) ((long) index * this.step / Constants.BITS_PER_BYTE)) / Constants.BYTES_PER_WORD,
+				defaultBuffer, defaultOffset, defaultSize);
+	}
+
+}
